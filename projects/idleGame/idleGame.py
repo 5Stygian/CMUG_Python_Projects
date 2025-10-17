23b683736ee7584e43744da0fb622c5ab872d548
5Stygian <rlcrafistohard@gmail.com>
2025-10-17 14:59:48 -0700

    start a new project

---
+++ projects/idleGame/idleGame.py
@@ -0,0 +1,15 @@
+from cmu_graphics import *
+
+import os
+
+
+
+#*****************************#
+#*****************************#
+#*****************************#
+
+os.system("clear")
+
+app.title = "idleGame"
+
+cmu_graphics.run() # type: ignore
