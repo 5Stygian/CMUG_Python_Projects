200d5f6a2970d5b194849e70f8188b1071441597
5Stygian <rlcrafistohard@gmail.com>
2025-10-24 13:32:32 -0700

    Update Button

--- projects/idleGame/cmuggui.py
+++ projects/idleGame/cmuggui.py
@@ -16,38 +16,38 @@ class Colors:
 class Button:
     def __init__(self, textContent: str,
                 x1: int|float, y1: int|float, x2: int|float, y2: int|float,
-                bgFill = rgb(200,200,200), bgBorder = rgb(180,180,180), bgBorderWidth = 2, bgVisible: bool = True,
+                hbFill = rgb(200,200,200), hbBorder = rgb(180,180,180), hbBorderWidth = 2, hbVisible: bool = True,
                 textFill = rgb(80,80,80), textFont: str = "arial", bold: bool = False, italic: bool = False, textVisible: bool = True):
-        
+
         self.textContent = textContent
-        
+
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
         self.y2 = y2
-        
-        self.bgFill          = bgFill
-        self.bgBorder        = bgBorder
-        self.bgBorderWidth   = bgBorderWidth
-        self.bgVisible       = bgVisible
-        
+
+        self.hbFill          = hbFill
+        self.hbBorder        = hbBorder
+        self.hbBorderWidth   = hbBorderWidth
+        self.hbVisible       = hbVisible
+
         self.textFill        = textFill
         self.textFont        = textFont
         self.bold            = bold
         self.italic          = italic
-        self.textVisible     = bgVisible
-        
+        self.textVisible     = textVisible
+
         self.hitbox = Polygon(
             self.x1,self.y1,
             self.x2,self.y1,
             self.x2,self.y2,
             self.x1,self.y2,
-            fill=self.bgFill,
-            border=self.bgBorder,
-            borderWidth=self.bgBorderWidth,
-            visible=self.bgVisible
+            fill=self.hbFill,
+            border=self.hbBorder,
+            borderWidth=self.hbBorderWidth,
+            visible=self.hbVisible
         )
-        
+
         self.text = Label(
             self.textContent,
             self.hitbox.centerX,self.hitbox.centerY,
@@ -57,23 +57,23 @@ class Button:
             italic=self.italic,
             visible=self.textVisible
         )
-    
+
         self.buttonGroup = Group( self.hitbox, self.text )
         self.buttonGroup.tl = [self.hitbox.x1, self.hitbox.y1]
         self.buttonGroup.tr = [self.hitbox.x2, self.hitbox.y2]
         self.buttonGroup.bl = [self.hitbox.x3, self.hitbox.y3]
         self.buttonGroup.br = [self.hitbox.x4, self.hitbox.y4]
-        
+
     # Only call when any of these attributes get changed
     def __updateAttrs(self) -> None:
         self.buttonGroup.tl = [self.hitbox.x1, self.hitbox.y1]
         self.buttonGroup.tr = [self.hitbox.x2, self.hitbox.y2]
         self.buttonGroup.bl = [self.hitbox.x3, self.hitbox.y3]
         self.buttonGroup.br = [self.hitbox.x4, self.hitbox.y4]
-       
+
     def getGroup(self) -> Group:
         return self.buttonGroup.children
-        
+
     def getData(self) -> Dict:
         returnDict = {
             "ClassName": self.__class__.__name__,
@@ -84,11 +84,11 @@ class Button:
                 "BottomLeft": self.buttonGroup.bl,
                 "BottomRight": self.buttonGroup.br
             },
-            "Background": {
-                "BackgroundFill": [self.bgFill.red,self.bgFill.green,self.bgFill.blue],
-                "BorderFill": [self.bgBorder.red,self.bgBorder.green,self.bgBorder.blue],
-                "BorderWidth": self.bgBorderWidth,
-                "IsVisible": self.bgVisible
+            "Hitbox": {
+                "BackgroundFill": [self.hbFill.red,self.hbFill.green,self.hbFill.blue],
+                "BorderFill": [self.hbBorder.red,self.hbBorder.green,self.hbBorder.blue],
+                "BorderWidth": self.hbBorderWidth,
+                "IsVisible": self.hbVisible
             },
             "Text": {
                 "TextContent": self.text.value,
@@ -102,34 +102,34 @@ class Button:
             }
         }
         return returnDict
-    
+
     def translate(self, x: int|float, y: int|float) -> Tuple:
         self.buttonGroup.centerX += x
         self.buttonGroup.centerY += y
         self.__updateAttrs()
         return (x, y)
-    
-    def rotate(self, degrees: int|float, 
-               xo: int|float|None = None, 
+
+    def rotate(self, degrees: int|float,
+               xo: int|float|None = None,
                yo: int|float|None = None) -> Tuple:
-        
+
         # set local variables
         if xo == None: xo = self.buttonGroup.centerX
         if yo == None: yo = self.buttonGroup.centerY
-        
+
         self.hitbox.rotate(degrees, xo, yo)
         self.text.rotate(degrees, xo, yo)
 
         self.__updateAttrs()
-        
+
         return (
             (self.hitbox.x1,self.hitbox.y1),
             (self.hitbox.x2,self.hitbox.y2),
             (self.hitbox.x3,self.hitbox.y3),
             (self.hitbox.x4,self.hitbox.y4)
         )
-        
-        
+
+
 # tests
 if __name__ == "__main__":
     testButton = Button(
@@ -138,10 +138,10 @@ if __name__ == "__main__":
         30,20
     )
     pp(testButton.getData(), sort_keys=False)
-    
+
     testButton.translate(100,100)
     pp(testButton.getData(), sort_keys=False)
-    
+
     testButton.rotate(90)
     pp(testButton.getData(), sort_keys=False)
 
--- projects/idleGame/idleGame.py
+++ projects/idleGame/idleGame.py
@@ -4,7 +4,13 @@ from assets import Colors, Button
 
 import os
 
+testButton = Button(
+    100,100,
+    200,200,
+    "this is a test"
+)
 
+print(testButton.getGroup())
 
 #*****************************#
 #*****************************#
